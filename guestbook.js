83f40dd39b6d31ad6767b23c8a99f79c5dfa8e3c
unknown <frostcolin863@gmail.com>
2025-06-14 17:37:51 +0200

    Added some Basic files

---
+++ guestbook.js
@@ -0,0 +1,6 @@
+const submit = document.getElementById('submit-button');
+
+submit.addEventListener("click") {
+    const nameInput = document.getElementById('name');
+    const messageInput = document.getElementById('message');
+}
