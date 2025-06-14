96d3b62f1c635d9cee163d16f5278a731bf93937
unknown <frostcolin863@gmail.com>
2025-06-14 18:23:50 +0200

    Addet Frontend Validation

--- guestbook.js
+++ guestbook.js
@@ -1,6 +1,12 @@
 const submit = document.getElementById('submit-button');
+const response = document.getElementById('response-message');
 
-submit.addEventListener("click") {
-    const nameInput = document.getElementById('name');
+submit.addEventListener("click", (event) => {
+    const nameInput = document.getElementById('name').value;
     const messageInput = document.getElementById('message');
-}
+    const matches = nameInput.match(/\d+/g);
+    if (matches != null) {
+        event.preventDefault();
+        response.innerHTML = "Your name cannot contain numbers.";
+    }
+});
